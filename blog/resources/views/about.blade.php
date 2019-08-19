cd5f205b3d5c33e712f87cfe975b023e1773eb7d
sanzetdas <paudelganesh10>
2019-08-19 18:22:27 +0545

    Routes added

--- blog/resources/views/about.blade.php
+++ blog/resources/views/about.blade.php
@@ -8,6 +8,8 @@
 </head>
 <body>
 <h1> About US</h1>
+<h2>Welcome to Site</h2>
+
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat reprehenderit quas cupiditate recusandae nulla maiores, saepe neque nostrum tempore alias non sunt vero dignissimos. Nihil in eligendi nemo odio ut.</p>
     <br/>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis nulla sed est placeat aliquam sapiente pariatur recusandae cupiditate similique, autem animi rerum iure alias? Est laboriosam vero saepe sint perferendis.</p>
