e7444354e17816843b452867f2f1608f16838a9b
sanzetdas <snzetdas@gmail.com>
2019-08-19 18:09:59 +0545

    Laravel Commit Startup and Routes

---
+++ blog/resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Us</title>
+</head>
+<body>
+<h1> About US</h1>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat reprehenderit quas cupiditate recusandae nulla maiores, saepe neque nostrum tempore alias non sunt vero dignissimos. Nihil in eligendi nemo odio ut.</p>
+    <br/>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis nulla sed est placeat aliquam sapiente pariatur recusandae cupiditate similique, autem animi rerum iure alias? Est laboriosam vero saepe sint perferendis.</p>
+</body>
+</html>
